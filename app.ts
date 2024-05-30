64c5b3ace6df03d6a343e8d10c7ce39559e81d08
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-31 02:59:19 +0500

    add a change

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+// ************** 100 coding challenge "2 day" ************** //
+
+
 /* Q4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name 
 of its author. Your output should look something like the following, including the quotation marks:
 "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"*/
