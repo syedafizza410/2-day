c1de021183daec434b0f8310334a6401bd4b68bb
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-30 15:17:56 +0500

    add

---
+++ app.ts
@@ -0,0 +1,25 @@
+/* Q4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name 
+of its author. Your output should look something like the following, including the quotation marks:
+"Albert Einstein once said, “A person who never made a mistake never tried anything new.”"*/
+
+console.log("Albert Einstein once said,\“A person who never made a mistake never tried anything new.\”")
+
+
+
+/* Q5 Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable 
+called famous_person. Then compose your message and store it in a new variable called message. 
+Print your message.*/
+
+
+let famous_person = "Albert Einstein";
+let message1 = `${famous_person} once said,\“A person who never made a mistake never tried anything new.\”`
+console.log(message1);
+
+/* Q6 Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and 
+end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the 
+whitespace around the name is displayed. Then print the name after stripping the white spaces.*/
+
+let personName1: string = "\t\n Ummefizza \t\n"; 
+
+console.log(personName1); 
+console.log(personName1.trim());
